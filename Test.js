999bf53b3140803fb8182b237a1f7864cfa877a9
Sri Vastav Polsani <vastavp@gmail.com>
2017-03-21 21:07:32 -0700

    Add files via upload

--- Test.js
+++ Test.js
@@ -1,3 +1,41 @@
 /**
  * Created by SriVastav on 3/19/2017.
  */
+
+describe("Validating title of a page",function(){
+
+
+    var title;
+    beforeEach(function () {
+
+        browser.get("http://www.way2automation.com/angularjs-protractor/banking/#/login");
+        title = browser.getTitle();
+        browser.sleep(2000);
+
+
+    });
+
+    it("Checking if title is same as passed value",function()
+    {
+        
+        title.then(function (txt) {
+
+            expect(txt).toEqual("Protractor practice website - Banking App");
+            
+        })
+
+    });
+
+    it("Checking if title is not equal to a value",function()
+    {
+
+        title.then(function (txt) {
+
+            expect(txt).not.toEqual("1Protractor practice website - Banking App");
+
+        })
+
+    });
+
+});
+
--- conf.js
+++ conf.js
@@ -2,5 +2,22 @@ exports.config = {
 directConnect: true,
 framework: 'jasmine2',
 seleniumAddress: 'http://localhost:4444/wd/hub',
-specs: ['TestMatchers.js']
+specs: ['Test.js'],
+    onPrepare: function(){
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+
+        allureReport: {
+            resultsDir: 'allure-results'
+        }
+    }));
+    jasmine.getEnv().afterEach(function(done){
+        browser.takeScreenshot().then(function(png){
+            allure.createAttachment('Screenshot', function(){
+                return new Buffer(png,'base64')
+            },'image/png')();
+            done();
+        })
+    });
+    }
 }
